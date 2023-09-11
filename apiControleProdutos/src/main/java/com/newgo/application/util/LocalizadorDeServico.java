f79742ce80cb8bc3d678d54aa98e7967799d92c5
Lucas Henrique <lucash100souza@gmail.com>
2023-09-11 11:15:12 -0300

    feat: Adicionado Padrão Service Locator como Alternativa à Injeção de Dependência
    Neste commit, foi introduzido o padrão Service Locator como uma alternativa à injeção de dependência para gerenciar a obtenção de serviços. As seguintes modificações foram realizadas:
    
    - Adicionada a classe `LocalizadorDeServico`, que atua como um localizador de serviços para obter instâncias de serviços necessários.
    - Implementados métodos no `LocalizadorDeServico` para permitir a obtenção do serviço de consulta de produtos.
    - Adicionado um método privado no `LocalizadorDeServico` responsável por criar uma conexão e instanciar o repositório de produtos.

---
+++ apiControleProdutos/src/main/java/com/newgo/application/util/LocalizadorDeServico.java
@@ -0,0 +1,29 @@
+package com.newgo.application.util;
+
+import com.newgo.domain.produto.ProdutoRepository;
+import com.newgo.domain.produto.useCases.ConsultarProduto;
+import com.newgo.infrastructure.configs.CarregaProperties;
+import com.newgo.infrastructure.conta.persistencia.ProdutoRepositoryPostgres;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class LocalizadorDeServico {
+
+    private static ProdutoRepository produtoRepository() throws SQLException {
+        return new ProdutoRepositoryPostgres(conexao());
+    }
+
+    public static ConsultarProduto consultaProduto() throws SQLException {
+        return new ConsultarProduto(produtoRepository());
+    }
+
+    private static Connection conexao() throws SQLException {
+        Properties properties = CarregaProperties.carregarProperties();
+        return DriverManager.getConnection(properties.getProperty("db.url"), properties.getProperty("db.usuario"),
+                properties.getProperty("db.senha"));
+    }
+
+}
