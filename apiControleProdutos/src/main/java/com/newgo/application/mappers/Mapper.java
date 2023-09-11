34ca80d51a8a67b9d88c6998d5d3806db86847f2
Lucas Henrique <lucash100souza@gmail.com>
2023-09-11 11:07:09 -0300

    feat: Adicionada Biblioteca ModelMapper para Conversão de DTOs
    Neste commit, foi adicionada a biblioteca ModelMapper ao projeto para facilitar a conversão entre objetos DTO (Data Transfer Object) e classes do domínio. As seguintes modificações foram realizadas:
    
    - Adicionada a dependência do ModelMapper ao arquivo "pom.xml" para que a biblioteca seja incluída no projeto.
    - Criada uma classe chamada "Mapper" para configurar e realizar as operações de mapeamento entre objetos DTO e classes de domínio.

---
+++ apiControleProdutos/src/main/java/com/newgo/application/mappers/Mapper.java
@@ -0,0 +1,20 @@
+package com.newgo.application.mappers;
+
+import org.modelmapper.ModelMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mapper {
+    private static ModelMapper mapper = new ModelMapper();
+
+    public static <O,D> D parseObject(O origin, Class<D> destination){
+        return  mapper.map(origin,destination);
+    }
+
+    public static <O,D> List<D> parseListObjects(List<O> origin, Class<D> destination){
+        List<D> destinationObjects = new ArrayList<D>();
+        origin.forEach( o -> destinationObjects.add(mapper.map(o,destination)));
+        return  destinationObjects;
+    }
+}
