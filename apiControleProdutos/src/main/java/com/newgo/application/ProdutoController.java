b08fb6797c553f88e37dba99dc1f53f153bb416d
Lucas Henrique <lucash100souza@gmail.com>
2023-09-13 09:50:42 -0300

    feat: Implementado Método doPost na Servlet para Cadastro de Novos Produtos e Adicionada Classe DTO CadastroProdutoDto Neste commit, foram realizadas as seguintes implementações:
    - Implementado o método doPost na Servlet: Foi adicionado o método doPost na Servlet para possibilitar o cadastro de novos produtos.
    - Adicionada a classe DTO CadastroProdutoDto: Foi criada a classe CadastroProdutoDto, que recebe os dados necessários para cadastrar um novo produto.

--- apiControleProdutos/src/main/java/com/newgo/application/ProdutoController.java
+++ apiControleProdutos/src/main/java/com/newgo/application/ProdutoController.java
@@ -1,17 +1,20 @@
 package com.newgo.application;
 
+import com.newgo.application.dto.Produto.CadastroProdutoDto;
 import com.newgo.application.dto.Produto.RespostaProdutoDto;
 import com.newgo.application.dto.RespostaExceptionDto;
 import com.newgo.application.mappers.Mapper;
 import com.newgo.application.util.LocalizadorDeServico;
-import com.newgo.application.util.json.ConverteParaJson;
+import com.newgo.application.util.json.ConversorJson;
 import com.newgo.domain.produto.Produto;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 
 @WebServlet("/produto")
@@ -24,7 +27,7 @@ public class ProdutoController extends HttpServlet {
             Produto produto = LocalizadorDeServico.consultaProduto().executar(hash);
             RespostaProdutoDto produtoDto = Mapper.parseObject(produto, RespostaProdutoDto.class);
 
-            String json = ConverteParaJson.converter(produtoDto);
+            String json = ConversorJson.converterToJson(produtoDto);
 
             resp.setContentType("application/json");
             resp.setStatus(200);
@@ -34,9 +37,43 @@ public class ProdutoController extends HttpServlet {
         }
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            String requestBody = getBodyReqJson(req);
+
+            CadastroProdutoDto cadastroProdutoDto = ConversorJson.converterParaObjeto(
+                    requestBody, CadastroProdutoDto.class);
+
+            Produto produto = Mapper.parseObject(cadastroProdutoDto, Produto.class);
+
+            Produto produtoCadastrado = LocalizadorDeServico.cadastrarProduto().executar(produto);
+
+            RespostaProdutoDto respostaProdutoDto = Mapper.parseObject(produtoCadastrado, RespostaProdutoDto.class);
+
+            String json = ConversorJson.converterToJson(respostaProdutoDto);
+
+            resp.setContentType("application/json");
+            resp.setStatus(201);
+            resp.getWriter().write(json);
+        } catch (Exception e) {
+            responderMensagemErro(resp, e);
+        }
+    }
+
+    private static String getBodyReqJson(HttpServletRequest req) throws IOException {
+        BufferedReader reader = req.getReader();
+        StringBuilder requestBody = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            requestBody.append(line);
+        }
+        return new String(requestBody.toString().getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
+    }
+
     private static void responderMensagemErro(HttpServletResponse resp, Exception e) throws IOException {
         RespostaExceptionDto respostaExceptionDto = new RespostaExceptionDto(e.getMessage(), LocalDateTime.now());
-        String json = ConverteParaJson.converter(respostaExceptionDto);
+        String json = ConversorJson.converterToJson(respostaExceptionDto);
         resp.setContentType("application/json");
         resp.setStatus(500);
         resp.getWriter().write(json);
